ab407b5ef9b87817313e711eb16999572e285cd2
yuanrui01 <xdyrfree@gmail.com>
2023-04-30 22:42:04 +0800

    spring6学习

---
+++ first/src/main/java/org/hypnos/spring6/Person.java
@@ -0,0 +1,13 @@
+package org.hypnos.spring6;
+
+/**
+ * @author yuanrui
+ * @email xdyrfree@gmail.com
+ * @date 2023/4/30-20:53
+ */
+public class Person {
+
+    public void hello(){
+        System.out.println("say Hello!");
+    }
+}
