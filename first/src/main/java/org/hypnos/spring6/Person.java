8de45c78642c2720e79d72ea5d7a3915053b9da8
yuanrui01 <xdyrfree@gmail.com>
2023-05-02 13:26:48 +0800

    spring6学习

--- first/src/main/java/org/hypnos/spring6/Person.java
+++ first/src/main/java/org/hypnos/spring6/Person.java
@@ -1,13 +1,30 @@
 package org.hypnos.spring6;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author yuanrui
  * @email xdyrfree@gmail.com
  * @date 2023/4/30-20:53
  */
+@Slf4j
 public class Person {
 
     public void hello(){
         System.out.println("say Hello!");
     }
+    @Test
+    void test(){
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("bean.xml");
+        Person person = (Person)classPathXmlApplicationContext.getBean("person");
+        person.hello();
+        
+        log.error("淄博烧烤");
+    }
+
 }
